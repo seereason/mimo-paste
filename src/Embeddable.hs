bbf11421b70e8609e92caa1f51c59abb44bed7e4
David Fox <dsf@seereason.com>
2015-08-08 05:50:35 -0700

    Remove bogus dependency, merge App.Orphans into App.Types

--- src/Embeddable.hs
+++ src/Embeddable.hs
@@ -7,7 +7,7 @@ module Embeddable where
 import Data.Time (UTCTime)
 import HSP (EmbedAsChild(asChild))
 import MIMO.ListForm (Column(Column))
-import App.Orphans (App')
+import App.Types (App')
 
 instance EmbedAsChild App' (Column UTCTime) where
     asChild (Column x) = asChild x
