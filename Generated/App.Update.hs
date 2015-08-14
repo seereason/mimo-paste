8c13cfdaaa8a9c3b7d8886ce6836f6d89ee753dc
David Fox <dsf@seereason.com>
2015-08-14 13:39:08 -0700

    Update generated files

--- Generated/App.Update.hs
+++ Generated/App.Update.hs
@@ -1,20 +1,23 @@
 instance Updatable App (TextArea Text)
-    where updForm x = (textarea 80 12 (unparse (fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                         0) (0,
-                                                                                                             0)))) x)) `transformEither` parse (fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                                                                                                                         0) (0,
-                                                                                                                                                                                                             0)))) x)) <++ errorList :: AppForm (TextArea Text)
+    where updForm x = (textarea 80 12 (unparse (fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                                        0) (0,
+                                                                                                            0)))) x)) `transformEither` parse (fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                                                                                                                                       0) (0,
+                                                                                                                                                                                                           0)))) x)) <++ errorList :: AppForm (TextArea Text)
+          updForm' = pure . updForm
 instance Updatable App TextFormat
     where updForm = \x -> select [(y,
-                                   show y) | y <- [minBound..maxBound]] (== fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                                                     0) (0,
-                                                                                                                                         0)))) x) :: AppForm TextFormat
+                                   show y) | y <- [minBound..maxBound]] (== fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                                                                    0) (0,
+                                                                                                                                        0)))) x) :: AppForm TextFormat
+          updForm' = pure . updForm
 instance Updatable App Paste
-    where updForm x = (set viewLens <$> updForm (Just (view viewLens (fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                                               0) (0,
-                                                                                                                                   0)))) x) :: Paste'))) <*> pure (fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                                                                                                                                            0) (0,
-                                                                                                                                                                                                                                0)))) x)
+    where updForm x = (set viewLens <$> updForm (Just (view viewLens (fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                                                              0) (0,
+                                                                                                                                  0)))) x) :: Paste'))) <*> pure (fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                                                                                                                                                          0) (0,
+                                                                                                                                                                                                                              0)))) x)
+          updForm' = pure . updForm
 instance Updatable App Paste'
     where updForm = \x -> mapView (\v -> [elt "span" <: fromStringLit "Paste'"] <> v) ((\(Just (Paste' _a₁
                                                                                                        _a₂
@@ -25,10 +28,12 @@ instance Updatable App Paste'
                                                                                                                        AppForm Paste') ((((((pure Paste' <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Paste Id'" ++ ": ")) <: form]) (updForm x)) (Just _a₁)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Title'" ++ ": ")) <: form]) (updForm x)) (Just _a₂)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Nickname'" ++ ": ")) <: form]) (updForm x)) (Just _a₃)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Format'" ++ ": ")) <: form]) (updForm x)) (Just _a₄)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Pasted'" ++ ": ")) <: form]) (updForm x)) (Just _a₅)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Paste'" ++ ": ")) <: form]) (updForm x)) (Just _a₆)) :: Maybe Paste' ->
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     AppForm Paste') x) :: Maybe Paste' ->
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           AppForm Paste'
+          updForm' = pure . updForm
 instance Updatable App PasteId
-    where updForm = \x -> fmap (const (fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                0) (0,
-                                                                                                    0)))) x)) (inputHidden empty :: AppForm Text)
+    where updForm = \x -> fmap (const (fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                               0) (0,
+                                                                                                   0)))) x)) (inputHidden empty :: AppForm Text)
+          updForm' = pure . updForm
 instance Updatable App PasteMeta
     where updForm = \x -> mapView (\v -> [elt "span" <: fromStringLit "Paste Meta"] <> v) ((\(Just (PasteMeta _a₁
                                                                                                               _a₂
@@ -37,9 +42,11 @@ instance Updatable App PasteMeta
                                                                                                                               AppForm PasteMeta) ((((pure PasteMeta <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Title" ++ ": ")) <: form]) (updForm x)) (Just _a₁)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Nickname" ++ ": ")) <: form]) (updForm x)) (Just _a₂)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Format" ++ ": ")) <: form]) (updForm x)) (Just _a₃)) <*> (\x -> mapView (\form -> [(elt "li" <: fromStringLit ("Pasted" ++ ": ")) <: form]) (updForm x)) (Just _a₄)) :: Maybe PasteMeta ->
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         AppForm PasteMeta) x) :: Maybe PasteMeta ->
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  AppForm PasteMeta
+          updForm' = pure . updForm
 instance Updatable App Text
-    where updForm x = (inputText (unparse (fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                    0) (0,
-                                                                                                        0)))) x)) `transformEither` parse (fromMaybe (error ("fromMaybe at " ++ show (Loc "" "" "" (0,
-                                                                                                                                                                                                    0) (0,
-                                                                                                                                                                                                        0)))) x)) <++ errorList :: AppForm Text
+    where updForm x = (inputText (unparse (fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                                   0) (0,
+                                                                                                       0)))) x)) `transformEither` parse (fromMaybe (error ("fromJust at " ++ show (Loc "" "" "" (0,
+                                                                                                                                                                                                  0) (0,
+                                                                                                                                                                                                      0)))) x)) <++ errorList :: AppForm Text
+          updForm' = pure . updForm
