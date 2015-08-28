d516e8ec0ce3567cf466c0d5e2afdc8c65b7af49
David Fox <dsf@seereason.com>
2015-08-28 10:39:32 -0700

    Update Debianize.hs

--- debian/Debianize.hs
+++ debian/Debianize.hs
@@ -1,7 +1,9 @@
+import Control.Lens (view)
 import Debian.AutoBuilder.Details.Versions (seereasonDefaults)
 import Debian.Debianize hiding (backups)
+import MIMO.App (spec)
 import MIMO.Debianize (customize)
-import Spec (spec)
+import Stage1Def (theAppInfo)
 
 main :: IO ()
-main = performDebianization (seereasonDefaults >> customize spec)
+main = performDebianization (seereasonDefaults >> customize (view spec theAppInfo))
